f25f4d1e485941eacd32ad29d68fce6f38c16025
fathrdm <fathiahnr570@gmail.com>
2024-06-30 19:54:32 +0800

    updata Auth Filters by Fathiah

--- Kelompok_1/app/Filters/Auth.php
+++ Kelompok_1/app/Filters/Auth.php
@@ -6,14 +6,11 @@ class Auth implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        // if user not logged in
         if(!session()->get('logged_in')){
-            // then redirct to login page
             return redirect()->to('/login'); 
         }
     }
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        // Do something here
     }
 }
