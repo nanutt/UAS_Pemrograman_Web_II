a25b812b01c2c81375342857ba96882851e91a0b
fathrdm <fathiahnr570@gmail.com>
2024-06-30 20:10:40 +0800

    update routes by fathiah

--- Kelompok_1/app/Config/Routes.php
+++ Kelompok_1/app/Config/Routes.php
@@ -1,42 +1,24 @@
 <?php
-
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-
- //auth
- $routes->post('/login/auth', 'Login::auth');
- $routes->post('/register/save', 'Register::save');
-
- //login
- $routes->get('login', 'Login::index');
- $routes->get('register', 'Register::index');
- $routes->post('register/save', 'Register::save');
- 
- //dash
- $routes->get('dashboard', 'views::index');
- $routes->get('response', 'views::e_response');
- 
- //tanya ft
- $routes->get('formpengajuan', 'views::form');
- $routes->get('tanyaft', 'views::tanya_ft');
- $routes->get('(:segment)/tanggapan', 'views::tanggapan/$1');
- $routes->get('(:segment)/preview', 'views::preview/$1');
- $routes->post('datapengajuan/save', 'views::save');
- 
- 
- 
- $routes->post('login/auth', 'Login::auth');
- $routes->get('logout', 'Login::logout');
- $routes->get('/', 'HalamanAwal::index');
-// $routes->group('buku', ['filter' => 'auth'], function ($routes) {
-//     $routes->get('index', 'BukuRoutes::index');
-//     $routes->get('(:segment)/preview', 'BukuRoutes::preview/$1');
-//     $routes->get('create', 'BukuRoutes::create');
-//     $routes->post('save', 'BukuRoutes::save');
-//     $routes->get('edit/(:segment)', 'BukuRoutes::edit/$1');
-//     $routes->post('update/(:segment)', 'BukuRoutes::update/$1');
-//     $routes->get('(:segment)/delete', 'BukuRoutes::delete/$1');
-// });
+//login
+$routes->get('login', 'Login::index');
+$routes->post('login/auth', 'Login::auth');
+$routes->get('logout', 'Login::logout');
+//Register
+$routes->get('register', 'Register::index');
+$routes->post('register/save', 'Register::save');
+//Layanan FT
+$routes->group('layananft', ['filter' => 'auth'], function ($routes) {
+    $routes->get('dashboard', 'Views::index');
+    $routes->get('response', 'Views::e_response');
+    $routes->get('formpengajuan', 'Views::form');
+    $routes->get('tanyaft', 'Views::tanya_ft');
+    $routes->get('(:segment)/tanggapan', 'Views::tanggapan/$1');
+    $routes->get('(:segment)/preview', 'Views::preview/$1');
+    $routes->post('datapengajuan/save', 'Views::save');
+});
+?>
