f37122cd67a0e28bcca5be0b71a03d3028a5f22c
dheaaprilinda01 <aprilindautami@gmail.com>
2024-06-30 19:49:06 +0800

    update AkunModel by dhea

--- Kelompok_1/app/Models/AkunModel.php
+++ Kelompok_1/app/Models/AkunModel.php
@@ -1,19 +1,14 @@
 <?php
 namespace App\Models;
-
 use CodeIgniter\Model;
 
 class AkunModel extends Model
 {
     protected $table = 'akun';
     protected $allowedFields = ['nama', 'email', 'no_hanphone', 'masuk_sebagai', 'password'];
-    public function get_data($nama, $password)
-    {
 
-        return $this->db->table('akun')
-            ->where(array('nama' => $nama, 'password' => $password))
-            ->get()->getRowArray();
+    public function get_data($nama)
+    {
+        return $this->where('nama', $nama)->first();
     }
 }
-
-?>
