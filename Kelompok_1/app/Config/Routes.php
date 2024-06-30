c382f6b9ed2bbc6a1a79839660fec2a9e2c56790
fathrdm <fathiahnr570@gmail.com>
2024-06-30 17:05:59 +0800

    update routes by Fathiah

--- Kelompok_1/app/Config/Routes.php
+++ Kelompok_1/app/Config/Routes.php
@@ -5,26 +5,34 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('login', 'Login::index');
-$routes->get('register', 'Register::index');
-$routes->post('register/save', 'Register::save');
-$routes->post('dashboard', 'views::index');
-$routes->get('(:segment)/preview', 'views::preview/$1');
-$routes->get('response', 'views::e_response');
-$routes->get('tanyaft', 'views::tanya_ft');
-$routes->post('datapengajuan/save', 'views::save');
-$routes->get('formpengajuan', 'views::form');
 
-
-$routes->post('dashboard', 'Login::auth');
-$routes->get('logout', 'Login::logout');
-$routes->get('/', 'HalamanAwal::index');
-$routes->group('buku', ['filter' => 'auth'], function ($routes) {
-    $routes->get('index', 'BukuRoutes::index');
-    $routes->get('(:segment)/preview', 'BukuRoutes::preview/$1');
-    $routes->get('create', 'BukuRoutes::create');
-    $routes->post('save', 'BukuRoutes::save');
-    $routes->get('edit/(:segment)', 'BukuRoutes::edit/$1');
-    $routes->post('update/(:segment)', 'BukuRoutes::update/$1');
-    $routes->get('(:segment)/delete', 'BukuRoutes::delete/$1');
-});
+ //login
+ $routes->get('login', 'Login::index');
+ $routes->get('register', 'Register::index');
+ $routes->post('register/save', 'Register::save');
+ 
+ //dash
+ $routes->get('dashboard', 'views::index');
+ $routes->get('response', 'views::e_response');
+ 
+ //tanya ft
+ $routes->get('formpengajuan', 'views::form');
+ $routes->get('tanyaft', 'views::tanya_ft');
+ $routes->get('(:segment)/tanggapan', 'views::tanggapan/$1');
+ $routes->get('(:segment)/preview', 'views::preview/$1');
+ $routes->post('datapengajuan/save', 'views::save');
+ 
+ 
+ 
+ $routes->post('login/auth', 'Login::auth');
+ $routes->get('logout', 'Login::logout');
+ $routes->get('/', 'HalamanAwal::index');
+// $routes->group('buku', ['filter' => 'auth'], function ($routes) {
+//     $routes->get('index', 'BukuRoutes::index');
+//     $routes->get('(:segment)/preview', 'BukuRoutes::preview/$1');
+//     $routes->get('create', 'BukuRoutes::create');
+//     $routes->post('save', 'BukuRoutes::save');
+//     $routes->get('edit/(:segment)', 'BukuRoutes::edit/$1');
+//     $routes->post('update/(:segment)', 'BukuRoutes::update/$1');
+//     $routes->get('(:segment)/delete', 'BukuRoutes::delete/$1');
+// });
