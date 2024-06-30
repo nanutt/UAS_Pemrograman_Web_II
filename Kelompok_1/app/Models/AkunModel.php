470e96f0317109587dd4e287ff4f3f6587381a4b
nanutt <najahmaisya03@gmail.com>
2024-06-30 17:10:09 +0800

    update AkunModel by Najah
    Co-authored-by: lenovo_mp2ce1jl@outlook.com <lenovo_mp2ce1jl@outlook.com>

--- Kelompok_1/app/Models/AkunModel.php
+++ Kelompok_1/app/Models/AkunModel.php
@@ -1,15 +1,19 @@
 <?php
 namespace App\Models;
+
 use CodeIgniter\Model;
+
 class AkunModel extends Model
 {
-            protected $table = 'akun';
-        protected $allowedFields = ['nama','email','no_hanphone', 'masuk_sebagai', 'password'];
-    public function get_data($nama, $no_hanphone, $email, $masuk_sebagai, $password)
+    protected $table = 'akun';
+    protected $allowedFields = ['nama', 'email', 'no_hanphone', 'masuk_sebagai', 'password'];
+    public function get_data($nama, $password)
     {
 
         return $this->db->table('akun')
-        ->where(array('nama' => $nama, 'password' => $password))
-        ->get()->getRowArray();
+            ->where(array('nama' => $nama, 'password' => $password))
+            ->get()->getRowArray();
+    }
 }
-}
+
+?>
